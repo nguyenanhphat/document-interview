813d3e03706c16fd423d95d8b64a3ed1f7ca7e9c
Nguyen Anh Phat <nguyenanhphat@Khoa-Nguyen-Mac-16.local>
2021-10-18 14:41:27 +0700

    update code

--- generator.js
+++ generator.js
@@ -1,8 +1,7 @@
 function* generateSequence() {
-  // yield 1;
+  yield 1;
   yield 2;
-  // return 3;
-  return;
+  return 3;
 }
 let sequence = [0, ...generateSequence()];
 // let one = generator.next();
@@ -17,6 +16,17 @@ function* gen() {
   let result = yield "2 + 2 = ?"; // (*)
   alert(result);
 }
-let generator = gen();
-let question = generator.next().value;
-generator.next(4);
+// let generator = gen();
+// let question = generator.next().value;
+// generator.next(4);
+
+let generator = generateSequence();
+
+const gen1 = generator.next();
+const gen2 = generator.next();
+// const gen3 = generator.next();
+// const gen4 = generator.next();
+// console.log('gen1---:', gen1);
+// console.log('gen2---:', gen2);
+// console.log('gen3---:', gen3);
+// console.log('gen4---:', gen4);
